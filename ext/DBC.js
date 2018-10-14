c427338fe4681f0dc8be6a17515e97dba4df1d38
Zen.Liu <lcz20@163.com>
2018-10-14 22:01:02 +0800

    1. improve websocket db service 2. add DBC lib for database use

--- ext/DBC.js
+++ ext/DBC.js
@@ -5,13 +5,13 @@ function DBC() {
         this._ready = true
     };
     this._pool = new Map();
-    this.database = new Map();
+    this._database = new Map();
     if (typeof DBC._initialized === "undefined") {
         DBC.prototype.send = function (message) {
             id = (new Date()).getTime();
             this._conn.send(id + `|` + message);
             return new Promise((rs, rj) => {
-                this._pool.set(id, {rs, rj})
+                this._pool.set(id, {rs: rs, rj: rj})
             });
         };
         DBC.prototype.receive = function (message) {
@@ -24,33 +24,33 @@ function DBC() {
             }
             this._pool.delete(id)
         };
-        DBC.prototype.open = function (name, password) {
-            this.database.forEach((v, k) => {
+        DBC.prototype.database = function (name) {
+            this._database.forEach((v, k) => {
                 if (k === name) {
                     return v
                 }
             });
-            this.database.set(name, function () {
-                this.send('OPEN|' + name + '|' + password);
-                return {
-                    name: name,
-                    query: (query) => {
-                        return this.send('QUERY|' + name + '|' + query)
-                    },
-                    exec: (query) => {
-                        return this.send('EXEC|' + name + '|' + query)
-                    },
-                    close: () => {
-                        this.database.delete(name);
-                        return this.send('CLOSE|' + name)
-                    },
-                    delete: () => {
-                        this.database.delete(name);
-                        return this.send('DELETE|' + name)
-                    }
+            this._database.set(name, {
+                name: name,
+                open: (password) => {
+                    return this.send('OPEN|' + name + '|' + password);
+                },
+                query: (query) => {
+                    return this.send('QUERY|' + name + '|' + query)
+                },
+                exec: (query) => {
+                    return this.send('EXEC|' + name + '|' + query)
+                },
+                close: () => {
+                    this._database.delete(name);
+                    return this.send('CLOSE|' + name)
+                },
+                delete: () => {
+                    this._database.delete(name);
+                    return this.send('DELETE|' + name)
                 }
             });
-            return this.database.get(name)
+            return this._database.get(name)
         };
         this._conn.onmessage = ev => {
             this.receive(ev.data)
@@ -63,4 +63,10 @@ function DBC() {
         };
         DBC._initialized = true;
     }
+}
+
+function TestDBC() {
+    let dbc=new DBC();
+    let db=dbc.database('local');
+    db.open('123').then((e)=>console.log(e)).catch((e)=>console.log(e));
 }
